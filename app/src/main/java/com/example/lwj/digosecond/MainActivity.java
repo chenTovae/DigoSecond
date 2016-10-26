9ce7038cb3e8e42aaada6cb7e265ae9477d16cbc
chenh <397314391@qq.com>
2016-10-26 19:01:01 +0800

    chenh first commit

--- app/src/main/java/com/example/lwj/digosecond/MainActivity.java
+++ app/src/main/java/com/example/lwj/digosecond/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String str ="大家好";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
